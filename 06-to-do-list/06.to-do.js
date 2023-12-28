e21224ced76ca5b60b178189b78e3fae2ba099ee
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-12-28 16:46:59 +0300

    Render To Do Items on Screen

--- 06-to-do-list/06.to-do.js
+++ 06-to-do-list/06.to-do.js
@@ -1,9 +1,22 @@
 const todoList = [];
 
+rendertodoList();
+
+function rendertodoList(){
+        let todoListHTML = '';
+        for (let i = 0; i < todoList.length; i++){
+                const todo = todoList[i];
+                const html = `<p>${todo}</p>`;
+                todoListHTML += html;
+        }
+        document.querySelector('.js-to-do-list').innerHTML = todoListHTML;
+}
+
 function addItem(){
         const inputElement = document.querySelector('.to-do-item');
         const item2Do = inputElement.value;
         todoList.push(item2Do);
         console.log(todoList);
         inputElement.value = '';
+        rendertodoList();
 };
